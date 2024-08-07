60ccf92bf852f1cd60a54fd80053cca4aa307aba
SolarX <vihaan.punjabi11@gmail.com>
2024-08-07 15:35:46 -0700

    Update data (1).py

--- data (1).py
+++ data (1).py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np  
 
-x_cords = [1] #add points by making a comma and an integer
+y_cords = [0] #add points by making a comma and an integer
 
-y_cords = [1] #add points by making a comma and an integer
-
+x_cords = [0] #add points by making a comma and an integer
 
 x_sum = sum(x_cords)
 
@@ -11,16 +11,22 @@ y_sum = sum(y_cords)
 
 slope = y_sum/x_sum
 y_intercept = 0
+
 for i in range(1, len(x_cords)):  
   y_intercept = y_intercept + (y_cords[i-1] - (slope * x_cords[i-1]))
 
 y = [(slope * x) + y_intercept for x in x_cords] 
 
+coefficients = np.polyfit(x_cords, y_cords, 1)  
+poly = np.poly1d(coefficients)
+y_regression = poly(x_cords)  
 plt.plot(x_cords, y_cords, label='Original Data')
 plt.plot(x_cords, y, label='MAD Line')
+plt.plot(x_cords, y_regression, label='Linear Regression')
 
 for x in x_cords:
-  plt.scatter(x, y_cords[x_cords.index(x)], color='red', label=('Point',x))
+  label_str = f"Point ({x}, {y_cords[x_cords.index(x)]})"
+  plt.scatter(x, y_cords[x_cords.index(x)], color='red', label=label_str)
 
 plt.legend()
 plt.show()
